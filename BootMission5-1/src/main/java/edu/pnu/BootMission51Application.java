eadb36cd53f87cc70282bfc5fa6f29e8662e77b3
nang5587 <nang5587@donga.ac.kr>
2025-04-15 17:58:23 +0900

    4/15

---
+++ BootMission5-1/src/main/java/edu/pnu/BootMission51Application.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMission51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMission51Application.class, args);
+	}
+
+}
