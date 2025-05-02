ade2959c1ea8fa1ad43e4a1cabbd4af98e961400
nang5587 <nang5587@donga.ac.kr>
2025-05-02 18:00:00 +0900

    5/2 login, exception

--- Chapter06/src/main/java/com/rubypaper/DataInit.java
+++ Chapter06/src/main/java/com/rubypaper/DataInit.java
@@ -6,17 +6,47 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import com.rubypaper.domain.Board;
+import com.rubypaper.domain.Member;
 import com.rubypaper.persistence.BoardRepository;
+import com.rubypaper.persistence.MemberRepository;
 
 @Component
 public class DataInit implements ApplicationRunner {
-	
+	@Autowired
+	private MemberRepository memberRepo;
 	@Autowired
 	private BoardRepository boardRepo;
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		for(int i=1; i<=20; i++) {
-			boardRepo.save(Board.builder().title("title " + i).writer("writer").content("content "+ i).build());
+//		for(int i=1; i<=20; i++) {
+//			boardRepo.save(Board.builder().title("title " + i).writer("writer").content("content "+ i).build());
+//		}
+		Member member1 = new Member();
+		member1.setId("member1");
+		member1.setName("둘리");
+		member1.setPassword("member111");
+		member1.setRole("ROLE_USER");
+		memberRepo.save(member1);
+		Member member2 = new Member();
+		member2.setId("member2");
+		member2.setName("도우너");
+		member2.setPassword("member222");
+		member2.setRole("ROLE_ADMIN");
+		memberRepo.save(member2);
+		for (int i = 1; i <= 3; i++) {
+			Board board = new Board();
+			board.setWriter("둘리");
+			board.setTitle("둘리가등록한게시글" + i);
+			board.setContent("둘리가등록한게시글내용" + i);
+			boardRepo.save(board);
+		}
+		for (int i = 1; i <= 3; i++) {
+			Board board = new Board();
+			board.setWriter("도우너");
+			board.setTitle("도우너가등록한게시글" + i);
+			board.setContent("도우너가등록한게시글내용" + i);
+			boardRepo.save(board);
 		}
 	}
 }
--- Chapter06/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter06/src/main/java/com/rubypaper/controller/BoardController.java
@@ -6,18 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.rubypaper.domain.Board;
+import com.rubypaper.domain.Member;
 import com.rubypaper.service.BoardService;
 
+@SessionAttributes("member")
 @Controller
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
+	@ModelAttribute("member")
+	public Member setMember() {
+		return new Member();
+	}
+	
 	@GetMapping("/getBoardList")
-	public String getBoardList(Model model) {
+	public String getBoardList(@ModelAttribute("member") Member member, Model model) {
+		if(member.getId() == null) {
+			return "redirect:login";
+		}
+		
 		List<Board> boardList = boardService.getBoardList();
 		
 		model.addAttribute("boardList", boardList);
@@ -52,5 +66,24 @@ public class BoardController {
 	public String deleteBoard(Board board) {
 		boardService.deleteBoard(board);
 		return "forward:getBoardList";
+	}
+	
+//	@GetMapping("/hello")
+//	public void hello(Model model) {
+//		model.addAttribute("greeting", "안녕 타임리프 !!");
+//	}
+	
+//	@GetMapping("/hello")
+//	public String hello(Model model) {
+//		model.addAttribute("greeting", "안녕 타임리프 !!");
+//		return "hello";
+//	}
+	
+	@GetMapping("/hello")
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("greeting", "안녕 타임리프 !!");
+		mv.setViewName("hello");
+		return mv;
 	}
 }
